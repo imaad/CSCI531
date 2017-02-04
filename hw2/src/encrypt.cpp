f2b0f04de337dac367400db8ad876211b7763f5d
imaad uk <imaad.ukaye@gmail.com>
2017-02-03 21:55:24 -0800

    encrypt code created

---
+++ hw2/src/encrypt.cpp
@@ -0,0 +1,13 @@
+//
+// Author : Imaad Ukaye
+// Course : CSCI-531
+// DATE   : 01/2 9/2017
+//
+#include <stdio.h>
+#include <string.h>
+#include <stdlib.h>
+#include <openssl/md5.h>
+
+// A simple stream cipher; outputs 'len' bytes of MD5 stream cipher
+void encrypt(char *pphrase, int len, int msg_type, unsigned char *out_buf) {
+}
