c04a983866bc3750bbabf93f45950d5b6186994d
imaad uk <imaad.ukaye@gmail.com>
2017-02-09 17:39:15 -0500

    hw2 commenting

--- hw2/src/hw2.cpp
+++ hw2/src/hw2.cpp
@@ -10,13 +10,16 @@
 #include "stream.h"
 #include "encrypt.h"
 #include "merge.h"
+#include "decrypt.h"
 
 char passPhrase[16];
 char stringLength[16];
 char strOut[16];
 char* mergeOutputFileName;
 int length = -1;
-void copy_string(char destination[], char source[], int skip) {
+
+// This method copies source characters to destination characters and skips the 'skip' number of characters in the source string
+void skipCopyString(char destination[], char source[], int skip) {
 	int c = 0;
 
 	while (source[c + skip] != '\0') {
@@ -25,17 +28,16 @@ void copy_string(char destination[], char source[], int skip) {
 	}
 	destination[c] = '\0';
 }
-// Main method of HW1
 char ERROR_MSG[] =
-		"Malformed command. Syntax: 'hw1 <hexdump|enc-base64|dec-base64) <fileName>'\n";
+		"Invalid arguments. The commandline syntax for hw2 is as follows:\nhw2 stream -p=pphrase -l=len \nhw2 encrypt -p=pphrase -out=name [pbmfile]\nhw2 merge pbmfile1 pbmfile2\nhw2 decrypt [pbmfile]";
 
 // Performs the required functionality after decoding input arguments
 void invokeApplication(int argc, char *argv[], int msgType) {
 	char ERROR_FILENOTFOUND[] = "Input file '%s' does not exist";
 	FILE *filePointerOne = NULL;
 	FILE *filePointerTwo = NULL;
-	memset(strOut,0,16);
-	// if the file name is provided in the command-line argument
+
+	// The following code initializes file handlers for each sub module and throws error if file is not found
 	if (msgType == 21) {
 		filePointerOne = stdin;
 	} else if (msgType == 22) {
@@ -52,31 +54,50 @@ void invokeApplication(int argc, char *argv[], int msgType) {
 		}
 
 		filePointerTwo = fopen(argv[3], "r");
-		if (filePointerOne == NULL) {
+		if (filePointerTwo == NULL) {
 			fprintf(stderr, ERROR_FILENOTFOUND, argv[3]);
 			exit(1);
 		}
 
+	} else if (msgType == 41) {
+		filePointerOne = stdin;
+	} else if (msgType == 42) {
+		filePointerOne = fopen(argv[2], "r");
+		if (filePointerOne == NULL) {
+			fprintf(stderr, ERROR_FILENOTFOUND, argv[2]);
+			exit(1);
+		}
+
 	}
+
 	// Calls the required modules based on decoded inputs from the command line
 	switch (msgType) {
 	case 1:
+		// Call Stream submodule
 		stream(passPhrase, length, msgType, NULL);
 		break;
 	case 21:
+		// Call encrypt submodule with stdin
 		encrypt(passPhrase, strOut, filePointerOne, msgType);
 		break;
 	case 22:
+		// Call encrypt submodule with file pointer
 		encrypt(passPhrase, strOut, filePointerOne, msgType);
 		break;
 	case 3:
-		merge(filePointerOne,filePointerTwo,msgType);
+		// Call merge submodule
+		merge(filePointerOne, filePointerTwo, msgType);
 		break;
 	case 41:
+		// Call decrypt submodule with stdin
+		decrypt(filePointerOne, msgType);
 		break;
 	case 42:
+		// Call decrypt submodule with file Pointer
+		decrypt(filePointerOne, msgType);
 		break;
 	default:
+		// If none of the above condition is met, throw error
 		fprintf(stderr, ERROR_MSG);
 		exit(1);
 
@@ -108,10 +129,10 @@ int parseInputs(int argc, char *argv[]) {
 			}
 			for (int i = 2; i < 4; ++i) {
 				if (strncmp(argv[i], "-p=", 3) == 0) {
-					copy_string(passPhrase, argv[i], 3);
+					skipCopyString(passPhrase, argv[i], 3);
 
 				} else if (strncmp(argv[i], "-l=", 3) == 0) {
-					copy_string(stringLength, argv[i], 3);
+					skipCopyString(stringLength, argv[i], 3);
 					length = atoi(stringLength);
 				} else {
 					fprintf(stderr, ERROR_MSG);
@@ -129,17 +150,15 @@ int parseInputs(int argc, char *argv[]) {
 			}
 			for (int i = 2; i < 4; ++i) {
 				if (strncmp(argv[i], "-p=", 3) == 0) {
-					copy_string(passPhrase, argv[i], 3);
+					skipCopyString(passPhrase, argv[i], 3);
 
 				} else if (strncmp(argv[i], "-out=", 5) == 0) {
-					copy_string(strOut, argv[i], 5);
+					skipCopyString(strOut, argv[i], 5);
 				} else {
 					fprintf(stderr, ERROR_MSG);
 					exit(1);
 				}
 			}
-			//printf("Pass: %s\n", passPhrase);
-			//printf("Out: %s\n", strOut);
 			if (argc == 4)
 				msg_type = 21;
 			else
@@ -163,14 +182,12 @@ int parseInputs(int argc, char *argv[]) {
 	}
 	return msg_type;
 }
+// Main method of HW2
 int main(int argc, char *argv[]) {
 
-//Look for correct command line arguments
-	//printf("ARGC: %d\n", argc);
-
+	//Look for correct command line arguments and generates a message type for each sub module.
 	int msg_type = parseInputs(argc, argv);
 	//Load the corresponding module based on Input arguments
-	//printf("The message Type is : %d\n", msg_type);
 	invokeApplication(argc, argv, msg_type);
 
 	return 0;
