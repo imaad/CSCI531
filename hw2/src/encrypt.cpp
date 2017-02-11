e6b565e384e4db697fe40b5a6979f83b7c49eb5e
imaad uk <imaad.ukaye@gmail.com>
2017-02-10 23:23:44 -0500

    2

--- hw2/src/encrypt.cpp
+++ hw2/src/encrypt.cpp
@@ -12,24 +12,27 @@
 //testing mode flag
 bool testing = true;
 
+// Prints a char string with \n in the end
 void printString(unsigned char str[]) {
-	int i = 0;
-	while (str[i] != '\0') {
-		printf("%c", str[i]);
-		i++;
+	int index = 0;
+	while (str[index] != '\0') {
+		printf("%c", str[index]);
+		index++;
 	}
 	printf("\n");
 }
 // Print the two-row output for a given file
-void print_rowout(FILE* out, unsigned char row_out[][2], int width) {
-	for (int i = 0; i < 2; i++) {
-		for (int j = 0; j < width; j++) {
-			fprintf(out, "%c", row_out[j][i]);
-		}
+void printEncryptedStream(FILE* outputPointer, unsigned char output[][2],
+		int width) {
+	for (int index = 0; index < width; index++) {
+		fprintf(outputPointer, "%c", output[index][0]);
+	}
+	for (int index = 0; index < width; index++) {
+		fprintf(outputPointer, "%c", output[index][1]);
 	}
 }
 
-// Open two new PBM files for writing and begin encryption
+// Generates file names for the encrypted files
 void generateFileNames(char* outputFileName, char** outputFileOne,
 		char** outputFileTwo) {
 
@@ -38,56 +41,60 @@ void generateFileNames(char* outputFileName, char** outputFileOne,
 	strcat(*outputFileOne, ".1.pbm");
 	strcat(*outputFileTwo, ".2.pbm");
 }
-void splitFiles(FILE* fout1, FILE* fout2, unsigned char* in_buf,
-		unsigned char* key_buf, int width, int height) {
-	unsigned char inputMask = 0x80;
-	unsigned char keyMask = 0x80;
-	unsigned char* inputBuffer;
-	unsigned char* keyBuffer;
+void generateDataFiles(FILE* filePointerOne, FILE* filePointerTwo,
+		unsigned char* inputStreamBuffer, unsigned char* keyStreamBuffer,
+		int width, int height) {
+
+	// Variable declaration
 	int inputBufferIndex = 0;
 	int keyBufferIndex = 0;
-
+	unsigned char inputMask = 0x80;
+	unsigned char keyMask = 0x80;
+	//Upper Limit Buffer Length
+	int bufferLength = (2 * width + 7) / 8;
 	unsigned int f1;
 	unsigned int f2;
-	unsigned char row_out1[(2 * width + 7) / 8][2]; // Function computes ceiling (2*width)/8 in bytes
-	unsigned char row_out2[(2 * width + 7) / 8][2];
-	// Iterate through each row of the PBM file
-	inputBuffer = (unsigned char*) (in_buf);
-	keyBuffer = (unsigned char*) (key_buf);
-	fprintf(fout1, "P4\n");
-	fprintf(fout1, "%d %d\n", width * 2, height * 2);
-	fprintf(fout2, "P4\n");
-	fprintf(fout2, "%d %d\n", width * 2, height * 2);
-	//printf("INP: ");
-	//printString(curr_in);
-	//printf("KEY: ");
-	//printString(curr_key);
+	unsigned char topRow[bufferLength][2];
+	unsigned char bottomRow[bufferLength][2];
+	unsigned char* inputBuffer;
+	unsigned char* keyBuffer;
+	inputBuffer = (unsigned char*) (inputStreamBuffer);
+	keyBuffer = (unsigned char*) (keyStreamBuffer);
+
+	// Prints file header
+	fprintf(filePointerOne, "P4\n");
+	fprintf(filePointerOne, "%d %d\n", width * 2, height * 2);
+	fprintf(filePointerTwo, "P4\n");
+	fprintf(filePointerTwo, "%d %d\n", width * 2, height * 2);
+	// Looping through the input row-wise
 	for (int heightIndex = 0; heightIndex < height; heightIndex++) {
-		// Iterate through each column, byte-wise
+		// Looping through each column
 		int index = 0;
 		for (int widthIndex = 0; widthIndex < (width + 7) / 8; widthIndex++) {
 
 			// Reset the encrypted bits for new file
+			// Top row
 			f1 = 0x00000000;
+			// Bottom row
 			f2 = 0x00000000;
-
 
 			// Iterate through each bit of every byte in the row
 			unsigned int whiteMask = 0x4000; // 0100 0000 0000 0000
 			unsigned int bigMask = 0x8000; // 1000 0000 0000 0000
-			for (int k = 0; k < 8; k++) {
-				if ((widthIndex * 8 + k) >= width) {
-					if (k == 4) {
-						// Printing don't cares from the start, just skip this byte
+			int bit = 0;
+			// Encrypt Twos bit at a time (Two for top, two for bottom)
+			while (bit < 8) {
+				if ((widthIndex * 8 + bit) >= width) {
+					if (!(bit ^ 4)) {
 						inputBufferIndex++;
 						inputMask = 0x80;
 						break;
 					}
 				} else {
-					// Identify whether the current bit is a 1 or 0
+					// The following code checks for possible combinations of white/black and key 0,1 and sets the corresponding encrypted bits specified by mask table
 					if ((unsigned int) ((inputBuffer[inputBufferIndex]
 							& inputMask)) > 0) {
-						// Black pixel
+						// If Black pixel is identified
 						if ((unsigned int) ((keyBuffer[keyBufferIndex] & keyMask))
 								> 0) {
 							// Black pixel, key=1
@@ -108,7 +115,7 @@ void splitFiles(FILE* fout1, FILE* fout2, unsigned char* in_buf,
 							f2 = ((((f2 & 0x0000ffff)) | bigMask)) | f2;
 						}
 					} else {
-						// White pixel
+						// White pixel is identified
 						if ((unsigned int) ((keyBuffer[keyBufferIndex] & keyMask))
 								> 0) {
 							// White pixel, key=1
@@ -131,58 +138,62 @@ void splitFiles(FILE* fout1, FILE* fout2, unsigned char* in_buf,
 
 						}
 					}
-					// Update key mask
+					// Shift key to use the next bit
 					keyMask = keyMask >> 1;
+					// if key is 0000 0000 0000 0000 then reset
 					if (!(keyMask & 0xff)) {
 						keyBufferIndex++;
 						keyMask = 0x80;
 					}
 				}
 				inputMask = inputMask >> 1;
+				// if input is 0000 0000 0000 0000 then reset
 				if (!(inputMask & 0xff)) {
 					inputBufferIndex++;
 					inputMask = 0x80;
 				}
+				//Shifting the mask by 2, to get the next 2x2 pixel block
 				whiteMask = whiteMask >> 2;
 				bigMask = bigMask >> 2;
+				bit++;
 			}
 			unsigned char byteOne;
-
 			unsigned char byteTwo;
+			// Converting the 4 byte into individual bytes to be represented as rows
 			byteOne = (f1 >> 24);
 			byteTwo = (f1 >> 16);
-			// Save char values from the shorts in correct order, based on endianness
-			row_out1[index][0] = byteOne;
-			row_out1[index + 1][0] = byteTwo;
+			topRow[index][0] = byteOne;
+			topRow[index + 1][0] = byteTwo;
 
 			byteOne = ((f1 & 0x0000ffff) >> 8) & 0xff;
 			byteTwo = ((f1 & 0x0000ffff)) & 0xff;
-			row_out1[index][1] = byteOne;
-			row_out1[index + 1][1] = byteTwo;
+			topRow[index][1] = byteOne;
+			topRow[index + 1][1] = byteTwo;
 
 			byteOne = (f2 >> 24);
 			byteTwo = (f2 >> 16);
-			row_out2[index][0] = byteOne;
-			row_out2[index + 1][0] = byteTwo;
+			bottomRow[index][0] = byteOne;
+			bottomRow[index + 1][0] = byteTwo;
 
 			byteOne = ((f2 & 0x0000ffff) >> 8) & 0xff;
 			byteTwo = ((f2 & 0x0000ffff)) & 0xff;
-			row_out2[index][1] = byteOne;
-			row_out2[index + 1][1] = byteTwo;
+			bottomRow[index][1] = byteOne;
+			bottomRow[index + 1][1] = byteTwo;
 
 			index += 2;
 		}
-		// Output the two rows to each file
-		print_rowout(fout1, row_out1, (2 * width + 7) / 8);
-		print_rowout(fout2, row_out2, (2 * width + 7) / 8);
+		// Print first row
+		printEncryptedStream(filePointerOne, topRow, bufferLength);
+		// Print Second row
+		printEncryptedStream(filePointerTwo, bottomRow, bufferLength);
 	}
 }
-void open_files(char *outputFileName, unsigned char *in_buf,
+void generateFileHeader(char *outputFileName, unsigned char *in_buf,
 		unsigned char *key_buf, long width, long height) {
 	FILE *fileOne, *fileTwo;
 
-	// Generate two filenames
-	char* outputFileOne = new char[100];		// Arbitrary filename length
+	// File Pointer for output files
+	char* outputFileOne = new char[100];
 	char* outputFileTwo = new char[100];
 
 	generateFileNames(outputFileName, &outputFileOne, &outputFileTwo);
@@ -190,57 +201,57 @@ void open_files(char *outputFileName, unsigned char *in_buf,
 	fileOne = fopen(outputFileOne, "w");
 	fileTwo = fopen(outputFileTwo, "w");
 
-	splitFiles(fileOne, fileTwo, in_buf, key_buf, width, height);
+	generateDataFiles(fileOne, fileTwo, in_buf, key_buf, width, height);
 
-	// We're done here, close up shop
+	// Free memory
 	delete[] outputFileOne;
 	delete[] outputFileTwo;
 	fclose(fileOne);
 	fclose(fileTwo);
 }
-void encrypt(char *pphrase, char *outputFileName, FILE *pnbFile, int msg_type) {
+void encrypt(char *p, char *outputFileName, FILE *inputFile, int msg_type) {
 
 	char fileHeader[2];
 	long width;
 	long height;
-	char *temp_buf = new char[100];
-	unsigned char *key_buf;
-	unsigned char *in_buf;
+	char *tempBuffer = new char[100];
+	unsigned char *keyBuffer;
+	unsigned char *inputBuffer;
 
-	// Make sure the magic number (P4) of the PBM file is correct
-	fgets(fileHeader, 3, pnbFile);
+	// Validating header
+	fgets(fileHeader, 3, inputFile);
 	//printf("%s", fileHeader);
 	if (0 != strcmp(fileHeader, "P4")) {
 		// Incorrect magic number
-		fprintf(stderr, "ERROR: invalid PBM file for input.\n");
+		fprintf(stderr, "Invalid PBM file\n");
 		return;
 	}
 	// Remove \n
-	fgets(temp_buf, 2, pnbFile);
+	fgets(tempBuffer, 2, inputFile);
 
 	// Read height/width values from PBM file
-	fgets(temp_buf, 10, pnbFile);
-	width = (int) strtol(temp_buf, &temp_buf, 10);
-	height = (int) strtol(temp_buf, NULL, 10);
+	fgets(tempBuffer, 10, inputFile);
+	// Extract width and height from the same line using strtol
+	width = (int) strtol(tempBuffer, &tempBuffer, 10);
+	height = (int) strtol(tempBuffer, NULL, 10);
+	// calculating input buffer size
 	long inputbufferSize =
 			(width % 8 == 0) ?
 					(height * width / 8) : (height * (width / 8 + 1));
-	// Read all data from input file
+	// Read the actual data
 	if (width % 8 == 0) {
-		in_buf = new unsigned char[inputbufferSize];
-		fread(in_buf, 1, (inputbufferSize), pnbFile);
+		inputBuffer = new unsigned char[inputbufferSize];
+		fread(inputBuffer, 1, (inputbufferSize), inputFile);
 	} else {
-		in_buf = new unsigned char[inputbufferSize];
-		fread(in_buf, 1, inputbufferSize, pnbFile);
+		inputBuffer = new unsigned char[inputbufferSize];
+		fread(inputBuffer, 1, inputbufferSize, inputFile);
 	}
 
-	// Read stream cipher
-	key_buf = new unsigned char[inputbufferSize];
-	stream(pphrase, inputbufferSize, msg_type, key_buf);
+	keyBuffer = new unsigned char[inputbufferSize];
+	// Generate a stream derieved from p to encrypt
+	stream(p, inputbufferSize, msg_type, keyBuffer);
 
 	// Open two files for input
-	open_files(outputFileName, in_buf, key_buf, width, height);
-
-// Free up the big memory buffers used
+	generateFileHeader(outputFileName, inputBuffer, keyBuffer, width, height);
 
 }
--- hw2/src/encrypt.h
+++ hw2/src/encrypt.h
@@ -11,6 +11,12 @@
 #define byteLength MD5_DIGEST_LENGTH/2
 
 // A simple stream cipher; outputs 'len' bytes of MD5 stream cipher
-void encrypt(char *pphrase, char *out, FILE *pnbFile, int msg_type);
-
-
+void encrypt(char *p, char *outputFileName, FILE *inputFile, int msg_type);
+void printString(unsigned char str[]);
+void printEncryptedStream(FILE* outputPointer, unsigned char output[][2],
+		int width);
+void generateFileNames(char* outputFileName, char** outputFileOne,
+		char** outputFileTwo);
+void generateDataFiles(FILE* filePointerOne, FILE* filePointerTwo,
+		unsigned char* inputStreamBuffer, unsigned char* keyStreamBuffer,
+		int width, int height);
